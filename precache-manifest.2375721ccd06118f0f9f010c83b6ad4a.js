a3a0dc58506aa1c9677c937510fea4107a0d5984
Jenny Cong <jfcong@berkeley.edu>
2019-05-03 23:02:13 -0700

    Updates

---
+++ precache-manifest.2375721ccd06118f0f9f010c83b6ad4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf06f8ec2b820e3ee6964b33a968141f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ca88de964103d2b54468",
+    "url": "/home/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "45bd243d7e3abb958841",
+    "url": "/home/static/css/main.c011fe85.chunk.css"
+  },
+  {
+    "revision": "ca88de964103d2b54468",
+    "url": "/home/static/js/2.8c1f44ed.chunk.js"
+  },
+  {
+    "revision": "45bd243d7e3abb958841",
+    "url": "/home/static/js/main.e729db94.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
